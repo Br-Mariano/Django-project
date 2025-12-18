c5fbdf8a3ba7f975a6a893ba04244b097e3e893a
Bruno Mariano <mariano020507@gmail.com>
2025-12-18 03:19:03 -0500

    Agregar .gitignore y limpiar archivos innecesarios

--- web/views.py
+++ web/views.py
@@ -185,7 +185,7 @@ def guardar_json(request):
 
                 # Guardar en sesión
                 request.session['usuario_id'] = usuario.id
-                return JsonResponse({"status": "ok", "mensaje": "Cuenta Creada en HFHFHFHDH"})
+                return JsonResponse({"status": "ok", "mensaje": "Cuenta Creada en GGGGGGGGGGGGGGGGGGGGGGG"})
 
             elif tipo == "loginData":
                 # Inicio de sesión
